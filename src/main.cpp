08359e8bba17680d4e2cd33359914ac0a48e7447
Claudio Teixeira <45026430+CTM1@users.noreply.github.com>
2022-05-24 17:32:18 +0200

    Base project structure and classes

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <CountingQF.hpp>
+#include <cstdio>
+
+int main(int argc, char ** argv)
+{
+    printf("The ducks in the park are free, you can take them home.\
+            I own 275 ducks.");
+}
