916a740d7ea2a5c4b23f981d1e4376ef718af68a
Claudio Teixeira <45026430+CTM1@users.noreply.github.com>
2022-05-25 16:09:48 +0200

    Small unitary testing and timing bechmark

--- src/main.cpp
+++ src/main.cpp
@@ -8,4 +8,7 @@ int main(int argc, char ** argv)
 {
     printf("The ducks in the park are free, you can take them home.\
             I own 275 ducks.\n");
+    CountingQF cqf = CountingQF(64);
+    testAsmRank(cqf);
+    testAsmSelect(cqf);
 }
