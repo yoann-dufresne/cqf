1fcf5247b274dd206862da405cd8d5ae1cd3c6fa
Claudio Teixeira <45026430+CTM1@users.noreply.github.com>
2022-05-25 12:56:21 +0200

    Basic program structure

--- src/CountingQF.cpp
+++ src/CountingQF.cpp
@@ -1,17 +1,27 @@
 #include <CountingQF.hpp>
 
-CountingQF::CountingQF(int elSize)
+#define DEFAULT_VEC_LEN 2048
+
+CountingQF::CountingQF(uint32_t elSize)
 {
     this -> elementSize = elSize;
-    this -> remainders = std::vector<uint64_t>(0);
-    this -> occupied = std::vector<bool>(false);
-    this -> runend = std::vector<bool>(false);
+
+    this -> remainders = std::vector<uint64_t>(DEFAULT_VEC_LEN, 0);
+    this -> occupied = std::vector<bool>(DEFAULT_VEC_LEN, false);
+    this -> runend = std::vector<bool>(DEFAULT_VEC_LEN, false);
+
+    this -> quotientSize = std::log2((double) DEFAULT_VEC_LEN);
+    this -> remainderSize = elSize - quotientSize;
 }
 
-CountingQF::CountingQF(int elSize, uint64_t sizeEstimate)
+CountingQF::CountingQF(uint32_t elSize, uint64_t vecSizeEstimate)
 {
     this -> elementSize = elSize;
-    this -> remainders = std::vector<uint64_t>(sizeEstimate, 0);
-    this -> occupied = std::vector<bool>(sizeEstimate, false);
-    this -> runend = std::vector<bool>(sizeEstimate, false);
+
+    this -> remainders = std::vector<uint64_t>(vecSizeEstimate, 0);
+    this -> occupied = std::vector<bool>(vecSizeEstimate, false);
+    this -> runend = std::vector<bool>(vecSizeEstimate, false);
+
+    this -> quotientSize = std::log2(vecSizeEstimate);
+    this -> remainderSize = elSize - quotientSize;
 }
