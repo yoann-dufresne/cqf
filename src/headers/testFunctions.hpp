45980b72f9a98c98b6f6e3af8a314dcec24df48e
Claudio Teixeira <45026430+CTM1@users.noreply.github.com>
2022-05-25 16:09:48 +0200

    Small unitary testing and timing bechmark

--- src/headers/testFunctions.hpp
+++ src/headers/testFunctions.hpp
@@ -1,27 +1,40 @@
 #ifndef TESTSUITE_HPP
 #define TESTSUITE_HPP
 
+#include <CountingQF.hpp>
 #include <cstdint>
 #include <cmath>
+#include <chrono>
 
 /**
  * @brief  Test if CQF asmRank funcition returns the number of 1s in
  * in uint64_t val up to position i including
- * @param val Testing Value
- * @param i Up to this index
+ * @param cqf CQF for testing.
  * @return true OK
  * @return false KO
  */
-bool testAsmRank(uint64_t val, int i);
+bool testAsmRank(CountingQF cqf);
 
 /**
  * @brief  Returns index of the nth 1 in val
  *
- * @param val Testing Value
- * @param i Up to this index
+ * @param cqf CQF for testing.
  * @return true OK
  * @return false KO
  */
-bool testAsmSelect(uint64_t val, int n);
+bool testAsmSelect(CountingQF cqf);
 
+/**
+ * @brief Times asmRank function calls
+ * 
+ * @param cqf CQF for testing
+ */
+void timeAsmRank(CountingQF cqf);
+
+/**
+ * @brief Times asmSelect function calls
+ * 
+ * @param cqf CQF for testing
+ */
+void timeAsmSelect(CountingQF cqf);
 #endif
