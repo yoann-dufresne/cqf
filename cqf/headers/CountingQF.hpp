f7d87d862718754b48a3ca1ac2d5d202f507972a
Claudio Teixeira <45026430+CTM1@users.noreply.github.com>
2022-07-26 15:25:03 +0200

    More work on iterator

--- cqf/headers/CountingQF.hpp
+++ cqf/headers/CountingQF.hpp
@@ -1,6 +1,9 @@
 #ifndef COUNTINGQF_HPP
 #define COUNTINGQF_HPP
 
+#define MEM_UNIT 8
+#define MAX_UINT 64
+
 #include <iostream>
 #include <iterator>
 #include <cstddef>
@@ -96,25 +99,64 @@ class CountingQF
             // We're iterating over the inserted values
             using value_type = uint64_t;
             
-            // Pointing to uint8_ts in our qf.
+            // Pointing to values in our qf.
             using pointer = uint8_t*;
             using reference = uint8_t&;
 
-        /*
-            Iterator(pointer ptr) : m_ptr(ptr) {}
+        
+            Iterator(CountingQF * c) {
+                cqf = c;
+                block_ptr = cqf -> qf;
+                mem_unit_ptr = cqf -> qf + 17;
+                curr_slot_abs = 0;
+                curr_slot_rel = 0;
+                curr_val = 0;
+            }
 
-            reference operator*() const { return *m_ptr; }
-            pointer operator->() const { return m_ptr; }
-            Iterator& operator++() { m_ptr++; return *this; }
+            reference operator*() const {
+                return (*mem_unit_ptr);    
+            }
+
+            pointer operator->() const {
+                return (mem_unit_ptr);
+            }
+        
+            Iterator& operator++() {
+                uint64_t bits_left = cqf -> remainder_len;
+                uint64_t first_bit_offset = (curr_slot_rel * cqf -> remainder_len) % MEM_UNIT; 
+
+                curr_slot_rel += 1;
+                curr_slot_abs += 1;
+
+                bits_left -= first_bit_offset;
+                mem_unit_ptr += 1;
+
+                while (bits_left > 8) {          
+                    mem_unit_ptr += 1;
+                    bits_left -= MEM_UNIT;
+                }
+
+                if (curr_slot_rel > 63) {
+                    curr_slot_rel %= MAX_UINT;
+                    mem_unit_ptr += 17;
+                }
+                
+                return *this;
+            }
+        /*
             Iterator operator++(int) { Iterator tmp = *this; ++(*this); return tmp; }
             friend bool operator== (const Iterator& a, const Iterator& b) { return a.m_ptr == b.m_ptr; };
             friend bool operator!= (const Iterator& a, const Iterator& b) { return a.m_ptr != b.m_ptr; }
         */
         public:
-            pointer block_ptr;
-            pointer mem_unit_ptr;
-            uint64_t curr_slot_rel;
-            uint64_t curr_slot_abs;
+            CountingQF * cqf;
+
+            uint8_t * block_ptr;
+            uint8_t * mem_unit_ptr;
+            
+            mutable uint64_t curr_slot_rel;
+            mutable uint64_t curr_slot_abs;
+            mutable uint64_t curr_val;
         };
 
         // Iterator begin() { return Iterator(&qf[0]); }
