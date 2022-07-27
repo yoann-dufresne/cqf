97e916fd603d24e0cae8ae3a5da0a862569b6541
Claudio Teixeira <45026430+CTM1@users.noreply.github.com>
2022-07-27 16:25:02 +0200

    Simplified CQFGetter

--- cqf/CQFGetter.cpp
+++ cqf/CQFGetter.cpp
@@ -1,5 +1,6 @@
 #include <CQFGetter.hpp>
 #include <CountingQF.hpp>
+#include <bitset>
 
 #define MEM_UNIT 8
 #define MAX_UINT 64
@@ -7,38 +8,18 @@
 CQFGetter::CQFGetter(CountingQF& cqf): cqf(cqf)
 {
     this -> cqf = cqf;
-    this -> block_ptr = cqf.qf;
-    this -> mem_unit_ptr = cqf.qf + 17;
     this -> curr_slot_abs = 0ULL;
-    this -> curr_slot_rel = 0ULL;
-    this -> curr_val = 0ULL;
 }
 
 uint64_t CQFGetter::get_current_value() {
-    curr_val = 0;
-    curr_val = cqf.get_rem_block(block_ptr, curr_slot_rel);
-    curr_val |= (curr_slot_abs << cqf.remainder_len);
+    uint64_t curr_val = 0;
+    
+    curr_val = cqf.get_rem(curr_slot_abs);
+    curr_val += (curr_slot_abs << cqf.remainder_len);
+
     return (curr_val);
 }
 
 void CQFGetter::next() {
-    uint64_t bits_left = cqf.remainder_len;
-    uint64_t first_bit_offset = (curr_slot_rel * cqf.remainder_len) % MEM_UNIT;
-    
-    curr_slot_rel += 1;
     curr_slot_abs += 1;
-    
-    bits_left -= first_bit_offset;
-    mem_unit_ptr += 1;
-    
-    while (bits_left > 8) {          
-        bits_left -= MEM_UNIT;
-        mem_unit_ptr += 1;
-    }
-
-    if (curr_slot_rel == MAX_UINT) {
-        curr_slot_rel %= MAX_UINT;
-        block_ptr += cqf.block_byte_size;
-        mem_unit_ptr = block_ptr + 17;
-    } 
 }
--- cqf/headers/CQFGetter.hpp
+++ cqf/headers/CQFGetter.hpp
@@ -6,12 +6,7 @@
 class CQFGetter {
     public:
         CountingQF &cqf;
-        uint8_t * block_ptr;
-        uint8_t * mem_unit_ptr;
-
-        uint64_t curr_slot_rel;
         uint64_t curr_slot_abs;
-        uint64_t curr_val;    
 
         explicit CQFGetter(CountingQF& cqf);
 
