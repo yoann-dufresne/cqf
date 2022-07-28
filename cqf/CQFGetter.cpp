26409cb5c00ed220476f62fc6de5447cf8ed194c
Claudio Teixeira <45026430+CTM1@users.noreply.github.com>
2022-07-28 17:12:47 +0200

    First implementation of CQFGetter

--- cqf/CQFGetter.cpp
+++ cqf/CQFGetter.cpp
@@ -1,5 +1,6 @@
 #include <CQFGetter.hpp>
 #include <CountingQF.hpp>
+#include <filterUtils.hpp>
 #include <bitset>
 
 #define MEM_UNIT 8
@@ -8,7 +9,11 @@
 CQFGetter::CQFGetter(CountingQF& cqf): cqf(cqf)
 {
     this -> cqf = cqf;
+    this -> curr_block_ptr = cqf.qf;
     this -> curr_slot_abs = 0ULL;
+    this -> curr_slot_rel = 0ULL;
+    this -> next_run_in_block = 0ULL;
+    this -> in_run = false;
 }
 
 uint64_t CQFGetter::get_current_value() {
@@ -21,5 +26,60 @@ uint64_t CQFGetter::get_current_value() {
 }
 
 void CQFGetter::next() {
-    curr_slot_abs += 1;
+    if (!in_run)
+        find_next_run();
+    else
+        next_run_val();                                                             
+}
+
+
+void CQFGetter::find_next_run() {
+    while (!in_run)
+    {
+        uint64_t * occupieds = ((uint64_t *) (curr_block_ptr + 1));
+
+        // Skip over empty blocks
+        while (*occupieds == 0) {
+            curr_block_ptr += cqf.block_byte_size;
+            occupieds = ((uint64_t *) (curr_block_ptr + 1));
+            curr_slot_rel = 0;
+            next_run_in_block = 0;
+            curr_slot_abs += MAX_UINT;
+        }
+
+        uint64_t old_slot_rel = curr_slot_rel;
+
+        // Get start of our next run
+        curr_slot_rel = asm_select(*occupieds, next_run_in_block);
+        next_run_in_block += 1;
+        
+        curr_slot_abs += curr_slot_rel - old_slot_rel;
+        
+        // If the start of our next run isn't present in this block
+        // Go to the next one and repeat steps
+        if (curr_slot_rel == MAX_UINT) {
+            curr_block_ptr += cqf.block_byte_size;
+            next_run_in_block = 0;
+            curr_slot_rel = 0;
+        }
+        else
+            in_run = true;
+    }              
+}
+
+
+void CQFGetter::next_run_val() {
+    curr_slot_rel += 1;
+    
+    // If the next value isn't in this block, go to the next one.
+    if (curr_slot_rel == MAX_UINT) {
+        curr_block_ptr += cqf.block_byte_size;
+        curr_slot_rel = 0;
+        next_run_in_block = 0;
+    }
+
+    uint64_t * runends = ((uint64_t *) (curr_block_ptr + 9));
+    
+    if (get_nth_bit_from(*runends, curr_slot_rel) == 1)
+        in_run = false;
 }
--- cqf/headers/CQFGetter.hpp
+++ cqf/headers/CQFGetter.hpp
@@ -7,10 +7,18 @@ class CQFGetter {
     public:
         CountingQF &cqf;
         uint64_t curr_slot_abs;
+        uint64_t curr_slot_rel;
+        uint64_t next_run_in_block;
+
+        uint8_t * curr_block_ptr;
+        bool in_run;
 
         explicit CQFGetter(CountingQF& cqf);
 
         uint64_t get_current_value();
         void next();
+
+        void find_next_run();
+        void next_run_val();
 };
 #endif
